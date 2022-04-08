9131a8e2ae6537b0c9b2d9de2660bb7fbab154ea
宇治 護 <sugoiyodarekasan@gmail.com>
2022-04-08 15:36:34 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# dynamic-novel-web
---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+
+const withTM = require('next-transpile-modules')(['three'])
+module.exports = withTM({
+  future: {
+    webpack5: true
+  }
+})
